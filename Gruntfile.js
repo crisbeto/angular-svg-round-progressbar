1f23afe79a7ad9b2f429ac88d32363684d74492b
crisbeto <kristiyan@basidia.dk>
2015-09-28 14:48:06 +0200

    more consistent coding style in the gruntfile, compared to the rest of the project. also add load-grunt-tasks for easier addition of new tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,60 +3,57 @@
 // jshint node:true
 
 module.exports = function(grunt) {
-  var files = [
-      'src/shim.js',
-      'src/module.js',
-      'src/roundProgressConfig.js',
-      'src/roundProgressService.js',
-      'src/roundProgress.js',
-  ];
+    var files = [
+        'src/shim.js',
+        'src/module.js',
+        'src/roundProgressConfig.js',
+        'src/roundProgressService.js',
+        'src/roundProgress.js',
+    ];
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    'uglify': {
-      'options': {
-        'banner': '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      'build': {
-        'src': files,
-        'dest': 'build/roundProgress.min.js'
-      }
-    },
-    'concat': {
-      'options': {
-        separator: '\n',
-      },
-      'build': {
-        src: files,
-        dest: 'build/roundProgress.js',
-      }
-    },
-    'gh-pages': {
-      'options': {
-        'base': 'build'
-      },
-      'deploy': {
-        'src': ['index.html','roundProgress.js', 'roundProgress.min.js']
-      }
-    },
-    'jshint': {
-      'options': {
-        'jshintrc': true,
-        'reporter': require('jshint-stylish')
-      },
-      'src': {
-        'files': {
-          'src': files
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: files,
+                dest: 'build/roundProgress.min.js'
+            }
+        },
+        concat: {
+            options: {
+                separator: '\n',
+            },
+            build: {
+                src: files,
+                dest: 'build/roundProgress.js',
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: 'build'
+            },
+            deploy: {
+                src: ['index.html','roundProgress.js', 'roundProgress.min.js']
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: true,
+                reporter: require('jshint-stylish')
+            },
+            src: {
+                files: {
+                    src: files
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-gh-pages');
-
-  grunt.registerTask('default', ['jshint:src', 'concat:build', 'uglify:build']);
-  grunt.registerTask('deploy', ['default', 'gh-pages:deploy']);
+    grunt.registerTask('default', ['jshint:src', 'concat:build', 'uglify:build']);
+    grunt.registerTask('deploy', ['default', 'gh-pages:deploy']);
 };
