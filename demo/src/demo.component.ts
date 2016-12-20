605dddaacbd1c08b6c3cf3cab83294ba1c21434e
crisbeto <crisbeto@abv.bg>
2016-12-20 17:10:13 +0200

    Add the animation selection to the demo.

--- demo/src/demo.component.ts
+++ demo/src/demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { RoundProgressEase } from 'round-progress';
 
 @Component({
   selector: 'demo',
@@ -19,13 +20,21 @@ export class DemoComponent {
   duration: number = 800;
   animation: string = 'easeOutCubic';
   animationDelay: number = 0;
-  animations: string[];
+  animations: string[] = [];
+
+  constructor(private _ease: RoundProgressEase) {
+    // Kinda hacky way to get all of the easing functions at run-time, because it can
+    // technically fetch something from up the prototype chain.
+    for (let prop in _ease) {
+      if (prop.toLowerCase().indexOf('ease') > -1) {
+        this.animations.push(prop);
+      };
+    }
+  }
 
   // TODO:
-  // - set up the animations
   // - add the gradient example
   // - add the current progress overlay
-
   increment(amount = 1) {
     this.current += amount;
   }
